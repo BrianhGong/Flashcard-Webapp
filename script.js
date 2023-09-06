1eed9e0dba7665ed19d3c0e09d6324a4eb678a37
Brian Gong <briangong0049@gmail.com>
2023-09-05 23:53:44 -0500

    Add script.js for navigation between html files
    Will be used to store flashcards as well

---
+++ script.js
@@ -0,0 +1,19 @@
+const boxButtons = document.querySelectorAll('.box');
+
+
+
+
+boxButtons.forEach(function(boxButton) {
+    boxButton.addEventListener('click', function() {
+        window.location.href = 'flashcard.html';
+    });
+});
+
+
+let flashcards =  {
+    
+}
+
+
+
+

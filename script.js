0545d89a8939c7a7757964d2593b0a9275f89eee
Brian Gong <briangong0049@gmail.com>
2023-09-06 22:43:52 -0500

    Add addflashcard.html for adding new flashcards

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const boxButtons = document.querySelectorAll('.box');
-
-
-
-
 boxButtons.forEach(function(boxButton) {
     boxButton.addEventListener('click', function() {
         window.location.href = 'flashcard.html';
     });
+});
+
+const addButton = document.querySelector("#add");
+addButton.addEventListener("click", () => {
+    window.location.href = "addflashcard.html";
 });
 
 

0c745e8b9d672eea3516e0e3698d873d0852ee3d
Brian Gong <briangong0049@gmail.com>
2023-09-11 22:55:13 -0500

    Add basic flashcard functionality in flashcard.html with flashcard.js
    App randomly picks flashcard to show with definition hidden. Show answer
    button shows definition. Know and Don't know button for Leitner System boxes

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+
+let box;
 const boxButtons = document.querySelectorAll('.box');
 boxButtons.forEach(function(boxButton) {
     boxButton.addEventListener('click', function() {
         window.location.href = 'flashcard.html';
+        box = boxButton.id;
+        
     });
 });
 
@@ -9,12 +13,5 @@ const addButton = document.querySelector("#add");
 addButton.addEventListener("click", () => {
     window.location.href = "addflashcard.html";
 });
-
-
-let flashcards =  {
-    
-}
-
-
 
 
